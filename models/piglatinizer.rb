a473aefbc785bf6190dade3f0c95d8242b55cb9f
osha7 <ogroetz@gmail.com>
2020-05-21 10:50:00 -0600

    Done.

---
+++ models/piglatinizer.rb
@@ -0,0 +1,28 @@
+class PigLatinizer
+
+    def piglatinize(word)
+        if word.split(" ").count == 1
+            piglatinize_word(word)
+        else
+            phrase = word.split(" ")
+            new_phrase = phrase.map {|w| piglatinize_word(w)}
+            new_phrase.join(" ")
+        end
+    end 
+
+    def piglatinize_word(word)
+        if word.downcase.start_with?(/[aeiou]/)
+            word + "way"
+        else
+            while word.downcase.start_with?(/[bcdfghjklmnpqrstvwxyz]/)
+                word = word.split('')
+                first_letter = word[0]
+                word.shift
+                word << first_letter
+                word = word.join('')
+            end
+            word + "ay"
+        end
+    end
+    
+  end
